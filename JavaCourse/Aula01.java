4b5f8d6175da032f0a46afb2398304cb564e2c8d
CaioStack <caiosalmarques7@gmail.com>
2026-08-07 19:39:54 -0300

    adiciona novo arquivo Aula01.java com exemplo de código

---
+++ JavaCourse/Aula01.java
@@ -0,0 +1,16 @@
+class aula01 {
+    public static void main(String[] args) {
+        System.out.println("Olá, Mundo!");
+
+
+        byte b = 100;
+        float f = 10.5f;
+        String str = "Caio";
+
+        if (b > 101) {
+            System.out.println("Verdadeiro");
+        } else {
+            System.out.println("Falso");
+        }
+    }
+}
