5454ada63025e19643d64040b08bc1f8fd539f43
Caio <caiosalmarques7@gmail.com>
2026-08-16 15:35:12 -0300

    Add files via upload

---
+++ JavaCourse/Exercícios/exercicio04.java
@@ -0,0 +1,41 @@
+/*
+ * ============================================================
+ *  EXERCÍCIO 04 - referente à AULA 04 (Estruturas de Repetição)
+ * ============================================================
+ * TAREFAS:
+ * 1) Use um for para imprimir todos os números pares de 1 a 50.
+ * 2) Use um while para somar todos os números de 1 a 100 e
+ *    imprima o resultado final (deve dar 5050).
+ * 3) Use um do-while para simular uma contagem regressiva de 10 até 0,
+ *    imprimindo "Fogo!" no final.
+ * 4) Usando break, faça um loop de 1 a 100 que pare assim que
+ *    encontrar o primeiro número divisível por 7 E por 3 ao mesmo tempo.
+ * 5) Usando loops aninhados, imprima um "triângulo" de asteriscos:
+ *    *
+ *    **
+ *    ***
+ *    ****
+ *    *****
+ *    (5 linhas, cada linha com uma quantidade crescente de *)
+ * ============================================================
+ */
+public class exercicio04 {
+
+    public static void main(String[] args) {
+
+        // TODO: Tarefa 1 - pares de 1 a 50
+
+
+        // TODO: Tarefa 2 - soma de 1 a 100 com while
+
+
+        // TODO: Tarefa 3 - contagem regressiva com do-while
+
+
+        // TODO: Tarefa 4 - break ao achar divisível por 7 e 3
+
+
+        // TODO: Tarefa 5 - triângulo de asteriscos
+
+    }
+}
