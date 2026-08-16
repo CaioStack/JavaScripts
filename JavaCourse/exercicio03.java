e8b3f92db9e4358cec0d21dbce12217d434db66d
Caio <caiosalmarques7@gmail.com>
2026-08-16 15:33:58 -0300

    Add files via upload

---
+++ JavaCourse/exercicio03.java
@@ -0,0 +1,39 @@
+/*
+ * ============================================================
+ *  EXERCÍCIO 03 - referente à AULA 03 (Estruturas Condicionais)
+ * ============================================================
+ * TAREFAS:
+ * 1) Peça (simule com uma variável) uma nota de 0 a 10 e, usando
+ *    if/else if/else, imprima o conceito: >=9 "Excelente", >=7
+ *    "Bom", >=5 "Regular", abaixo disso "Insuficiente".
+ * 2) Usando o operador ternário, verifique se um número é positivo,
+ *    negativo ou zero (dica: pode precisar de ternário aninhado).
+ * 3) Crie um switch (tradicional OU moderno) que receba um número de
+ *    1 a 12 e imprima o nome do mês correspondente.
+ * 4) Verifique se uma pessoa pode entrar em uma balada usando
+ *    condições compostas: precisa ter 18 anos OU MAIS e estar com
+ *    documento (boolean temDocumento = true/false).
+ * ============================================================
+ */
+public class exercicio03 {
+
+    public static void main(String[] args) {
+
+        // TODO: Tarefa 1 - conceito a partir da nota
+        double nota = 8.5;
+
+
+        // TODO: Tarefa 2 - ternário positivo/negativo/zero
+        int numero = -5;
+
+
+        // TODO: Tarefa 3 - switch com nome do mês
+        int mes = 6;
+
+
+        // TODO: Tarefa 4 - entrada na balada
+        int idade = 20;
+        boolean temDocumento = true;
+
+    }
+}
